362e87cc927a91f8397f827cf1ddb7bde5af08e1
youstinus <youstinus@gmail.com>
2019-03-08 20:14:14 +0200

    (5) Projects page added. Fixed routing in app-routing.module.ts. Added organizations list page. Added auth guard with no logic yet for authenticating users for some of the functionality. Projects service added dummy token for requests to back end. Projects page have test data but posible to get data from back-end.

--- ionic-ui/src/app/app-routing.module.ts
+++ ionic-ui/src/app/app-routing.module.ts
@@ -1,20 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'project', loadChildren: './pages/project/project.module#ProjectPageModule' },
-  { path: 'nav-menu', loadChildren: './pages/nav-menu/nav-menu.module#NavMenuPageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'user-settings', loadChildren: './pages/user-settings/user-settings.module#UserSettingsPageModule' },
-  { path: 'organization', loadChildren: './pages/organization/organization.module#OrganizationPageModule' },
-  { path: 'registration', loadChildren: './pages/registration/registration.module#RegistrationPageModule' },
-  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', loadChildren: './home/home.module#HomePageModule'},
+    {path: 'nav-menu', loadChildren: './pages/nav-menu/nav-menu.module#NavMenuPageModule'},
+    {path: 'login', loadChildren: './pages/login/login.module#LoginPageModule'},
+    {path: 'user-settings', loadChildren: './pages/user-settings/user-settings.module#UserSettingsPageModule'},
+    {
+        path: 'organizations', children: [
+            {path: '', loadChildren: './pages/organizations/organizations.module#OrganizationsPageModule'},
+            {path: ':id', loadChildren: './pages/organization/organization.module#OrganizationPageModule'}
+        ]
+    },
+    {path: 'registration', loadChildren: './pages/registration/registration.module#RegistrationPageModule'},
+    {path: 'about', loadChildren: './pages/about/about.module#AboutPageModule'},
+    {
+        path: 'projects', children: [
+            {path: '', loadChildren: './pages/projects/projects.module#ProjectsPageModule'},
+            {path: ':id', loadChildren: './pages/project/project.module#ProjectPageModule'}
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
