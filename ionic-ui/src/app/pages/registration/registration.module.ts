6e257bbbe18155f94b23a523997bfda71238deaf
Brigita <brigitavilei@gmail.com>
2019-03-12 20:22:10 +0200

    Worked on register form and page content

--- ionic-ui/src/app/pages/registration/registration.module.ts
+++ ionic-ui/src/app/pages/registration/registration.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController, AlertController } from '@ionic/angular';
 
 import { RegistrationPage } from './registration.page';
 
@@ -23,4 +23,50 @@ const routes: Routes = [
   ],
   declarations: [RegistrationPage]
 })
-export class RegistrationPageModule {}
+export class RegistrationPageModule {
+
+  createSuccess = false;
+  registerCredentials = { name: '', email: '', password: '', confirmation_password: '' };
+
+  constructor(
+    private nav: NavController,
+   // private auth: AuthServiceProvider,
+    private alertCtrl: AlertController
+  ) {}
+
+  /*public register() {
+    if (this.registerCredentials.password != this.registerCredentials.confirmation_password) {
+      this.showPopup("Error", 'The password confirmation does not match.');
+    } else {
+      this.auth.register(this.registerCredentials).subscribe(success => {
+        if (success) {
+          this.createSuccess = true;
+          this.showPopup("Success", "Account created.");
+        } else {
+          this.showPopup("Error", "Problem creating account.");
+        }
+      },
+        error => {
+          this.showPopup("Error", error);
+        });
+    }
+  }*/
+  /*showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: [
+        {
+          text: 'OK',
+          handler: data => {
+            if (this.createSuccess) {
+              this.nav.popToRoot();
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }*/
+
+}
