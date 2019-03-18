cd20dad064504ad5a7014c5ae294322a79368fd3
ignnar <ignas.narusis@gmail.com>
2019-03-18 23:03:13 +0200

    (6) Finished with project.page; Added features: +Contact info +Project date +-Volunteer count +button that changes color to confirm a user that he saved a page, was added to volunteer list

--- ionic-ui/src/app/pages/project/project.page.ts
+++ ionic-ui/src/app/pages/project/project.page.ts
@@ -1,19 +1,47 @@
 import {Component, OnInit} from '@angular/core';
 import {Project} from 'src/app/models/Project';
-
+import { NavController, IonButton } from '@ionic/angular';
+import { Button } from 'protractor';
+// import { Clipboard } from '@ionic-native/clipboard/ngx';
 @Component({
     selector: 'app-project',
     templateUrl: './project.page.html',
     styleUrls: ['./project.page.scss'],
 })
+
 export class ProjectPage implements OnInit {
 
-    constructor() {
+    constructor(public navCtrl: NavController) {
+        
     }
-
+    btnActivate(ionicButton) {
+        if(ionicButton.color === 'dark')
+          ionicButton.color =  'success';
+        else if(ionicButton.color == 'success')
+          ionicButton.color = 'dark';
+          else
+            ionicButton.color = "success";
+            
+            console.log(ionicButton);
+      }
+    
+      isSelected(event) {
+        console.log(event);
+        return 'primary';
+        // event.target.getAttribute('selected') ? 'primary' : '';
+      }
+    btnClicked()
+    {
+        alert("This page successfully saved");
+        
+    }
+     
     ngOnInit() {
     }
+    labClick()
+    {
 
+    }
     private project: Project = {
         id: 1,
         title: 'VšĮ Pagirk',
@@ -24,7 +52,38 @@ export class ProjectPage implements OnInit {
         picturesIds: [1],
         start: new Date('2019-05-01'),
         end: new Date('2019-05-01'),
-        volunteersIds: [1],
+        volunteersIds: [2],
         website: 'https://volunteering.com'
     };
+}
+export class PopoverComponent {
+
+
+    public ionicNamedColor: string = 'primary';
+    color: any;
+    submit(event) {
+        let prevColor = this.color;
+        if (this.color === 'primary') {
+                this.color = 'light'
+        } else {
+                this.color = 'primary'
+        }
+    
+        if (event.target.localName === 'button') {
+            event.target.className =event.target.className.replace('button-md-' + prevColor, 'button-md-' + this.color);
+        } else if (event.target.parentElement.localName === 'button') {
+            event.target.parentElement.className = event.target.parentElement.className.replace('button-md-' + prevColor, 'button-md-' + this.color);
+        }
+    }
+    constructor() {
+    }
+    
+    public toggleNamedColor(): void {
+      if (this.ionicNamedColor === 'primary') {
+        this.ionicNamedColor = 'light'
+      } else {
+        this.ionicNamedColor = 'primary'
+      }
+    }
+    
 }
