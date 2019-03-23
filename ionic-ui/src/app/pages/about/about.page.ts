141ba0eace464ca661e1ecbe854f508ee408cc8c
Brigita <brigitavilei@gmail.com>
2019-03-23 22:52:10 +0200

    (16) Worked on about us page. Created comment form, but the data is still stored in page after committing. Inserted link cards, but would like them to navigate into specific page Added additional information

--- ionic-ui/src/app/pages/about/about.page.ts
+++ ionic-ui/src/app/pages/about/about.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NavController, MenuController, ToastController, AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutPage implements OnInit {
 
-  constructor() { }
+  public commentForm: FormGroup;
+  sources: string[];
+
+  constructor(
+    public navCtrl: NavController,
+    public menuCtrl: MenuController,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder,
+
+  ) { }
 
   ngOnInit() {
+
+    this.sources=
+    [
+      'https://www.gvi.co.uk/blog/17-excellent-reasons-to-volunteer/',
+      'https://buildabroad.org/2017/10/13/why-is-volunteering-important/'
+    ];
+
+    this.commentForm = this.formBuilder.group({
+      'email': [null, Validators.compose([
+        Validators.required ,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])],
+      'text': [null, Validators.compose([
+        Validators.required
+      ])]
+    });
+  }
+
+  leaveComment(){
+    console.log(this.commentForm.value);
+    this.commentResult();
+  }
+
+  async commentResult() {
+    const alert = await this.alertCtrl.create({
+      header: 'Thank you',
+      message: 'Your email has been submited',
+      buttons: [
+        {
+          text: 'Continue',
+          role: 'Continue',
+          cssClass: 'secondary',
+          handler: () => {
+            // noreciau kad duomenys issitrintu?
+            console.log('Confirm continue');
+            
+            this.commentForm.setValue({
+              'email': [null],
+              'text' : [null]
+            });
+            
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  onSourceClicked(source: string){
+    console.log('Gal pavyktu kaip nors cia nauja page atidaryt? ');
+    //this.navCtrl.navigateForward(source);
   }
 
 }
