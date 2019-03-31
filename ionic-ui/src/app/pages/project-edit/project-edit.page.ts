dd19e353505f20098acee8ac7a2967335a77cc8e
ignnar <ignas.narusis@gmail.com>
2019-03-31 23:13:43 +0300

    (24) Created project edit page and some basic changes to project page.

---
+++ ionic-ui/src/app/pages/project-edit/project-edit.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-edit',
+  templateUrl: './project-edit.page.html',
+  styleUrls: ['./project-edit.page.scss'],
+})
+export class ProjectEditPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
