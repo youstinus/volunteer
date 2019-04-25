d3a40a46bd59b5f9f94537ed47eafb4897b9fa3f
youstinus <youstinus@gmail.com>
2019-04-25 22:25:12 +0300

    Commit for travis ci

--- scripts/docker-publish-heroku.bat
+++ scripts/docker-publish-heroku.bat
@@ -1,12 +1,12 @@
 SET DOCKER_ENV=production
 SET DOCKER_TAG=latest
 
-heroku login -i %HEROKU_USERNAME% %HEROKU_PASSWORD%
+heroku login -i $HEROKU_USERNAME $HEROKU_PASSWORD
 
-heroku container:login -u %HEROKU_USERNAME% -p %HEROKU_PASSWORD%
+heroku container:login -u $HEROKU_USERNAME -p $HEROKU_PASSWORD
 
-docker build -f ./server/WebAPI/Dockerfile.%DOCKER_ENV% -t volunteer-webapi:%DOCKER_TAG% ./server/WebAPI/bin/Docker --no-cache
+docker build -f ./server/WebAPI/Dockerfile.$DOCKER_ENV -t volunteer-webapi:$DOCKER_TAG ./server/WebAPI/bin/Docker --no-cache
 
-docker tag volunteer-webapi:%DOCKER_TAG% registry.heroku.com/volunteer-webapi/web:%DOCKER_TAG%
+docker tag volunteer-webapi:$DOCKER_TAG registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
 
-docker push registry.heroku.com/volunteer-webapi/web:%DOCKER_TAG%
+docker push registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
--- scripts/docker-publish-travis.bat
+++ scripts/docker-publish-travis.bat
@@ -1,10 +1,10 @@
 SET DOCKER_ENV=production
 SET DOCKER_TAG=latest
 
-docker login -u %DOCKER_USERNAME% -p %DOCKER_PASSWORD%
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker build -f ./server/WebAPI/Dockerfile.%DOCKER_ENV% -t volunteer-webapi:%DOCKER_TAG% ./server/WebAPI/bin/Docker --no-cache
+docker build -f ./server/WebAPI/Dockerfile.$DOCKER_ENV -t volunteer-webapi:$DOCKER_TAG ./server/WebAPI/bin/Docker --no-cache
 
-docker tag volunteer-webapi:%DOCKER_TAG% %DOCKER_USERNAME%/volunteer-webapi:%DOCKER_TAG%
+docker tag volunteer-webapi:$DOCKER_TAG $DOCKER_USERNAME/volunteer-webapi:$DOCKER_TAG
 
-docker push %DOCKER_USERNAME%/volunteer-webapi:%DOCKER_TAG%
+docker push $DOCKER_USERNAME/volunteer-webapi:$DOCKER_TAG
