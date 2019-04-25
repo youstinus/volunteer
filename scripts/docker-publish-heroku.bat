b422ecdb8dcd42a025133f0abe067bebb436a6a1
youstinus <youstinus@gmail.com>
2019-04-26 00:56:50 +0300

    travis

--- scripts/docker-publish-heroku.bat
+++ scripts/docker-publish-heroku.bat
@@ -6,5 +6,3 @@ docker build -f ./server/WebAPI/Dockerfile.$DOCKER_ENV -t volunteer-webapi:$DOCK
 docker tag volunteer-webapi:$DOCKER_TAG registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
 
 docker push registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
-
-heroku container:release web -a volunteer-webapi
