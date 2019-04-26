d4f10026c162e7c88de8f388632a4720c5fc4320
youstinus <youstinus@gmail.com>
2019-04-26 08:03:20 +0300

    travis commit

--- scripts/docker-publish-heroku.bat
+++ scripts/docker-publish-heroku.bat
@@ -1,8 +1,8 @@
 
 docker login --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com
 
-docker build -f ./server/WebAPI/Dockerfile.$DOCKER_ENV -t volunteer-webapi:$DOCKER_TAG ./server/WebAPI --no-cache
-
-docker tag volunteer-webapi:$DOCKER_TAG registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
+docker build -f ./server/WebAPI/Dockerfile.$DOCKER_ENV -t registry.heroku.com/volunteer-webapi/web ./server/WebAPI --no-cache
 
 docker push registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
+
+heroku container:release web -a volunteer-webapi
