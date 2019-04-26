9aba276e4261cca77d266c241b10c37acd195217
youstinus <youstinus@gmail.com>
2019-04-26 08:31:00 +0300

    travis

--- scripts/docker-publish-heroku.bat
+++ scripts/docker-publish-heroku.bat
@@ -1,8 +1,10 @@
 
-docker login --username=_ --password=$HEROKU_AUTH_TOKEN registry.heroku.com
+echo "$HEROKU_AUTH_TOKEN" | docker login --username=_ registry.heroku.com --password-stdin
 
 docker build -f ./server/WebAPI/Dockerfile.$DOCKER_ENV -t registry.heroku.com/volunteer-webapi/web ./server/WebAPI --no-cache
 
 docker push registry.heroku.com/volunteer-webapi/web:$DOCKER_TAG
 
-echo "$DOCKER_USERNAME $DOCKER_PASSWORD" | heroku container:release web -a volunteer-webapi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+heroku container:release web -a volunteer-webapi
