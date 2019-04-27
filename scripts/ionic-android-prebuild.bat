458312c0a8ca03fbe3dcb1b8fef692550b4ca4d1
youstinus <youstinus@gmail.com>
2019-04-27 14:20:35 +0300

    again

--- scripts/ionic-android-prebuild.bat
+++ scripts/ionic-android-prebuild.bat
@@ -1,13 +1,13 @@
 
 cd ionic-ui
-nvm install 7
+nvm install 8
 node --version
 travis_retry curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 travis_retry sudo apt-get update -qq
 travis_retry sudo apt-get install -y -qq yarn
 yarn
-npm install -g gulp bower cordova ionic
+npm install -g cordova ionic
 npm install
 
 #bower update
