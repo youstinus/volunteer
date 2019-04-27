d93a2d69a537bde63c68f34742be6c17c9c24ac5
youstinus <youstinus@gmail.com>
2019-04-27 13:24:26 +0300

    try two languages

--- scripts/ionic-android-build.bat
+++ scripts/ionic-android-build.bat
@@ -5,6 +5,6 @@ mkdir -p output
 set -e
 #cordova platform add android --nofetch
 
-ionic cordova build android
+ionic cordova build android --verbose
 
 cp platforms/android/app/build/outputs/apk/debug/app-debug.apk output/app.apk
