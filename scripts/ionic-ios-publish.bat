f2ba7b9b6b9c4ac7765c46b039da4f58c1d33b06
youstinus <youstinus@gmail.com>
2019-04-27 23:09:00 +0300

    Added tags true

--- scripts/ionic-ios-publish.bat
+++ scripts/ionic-ios-publish.bat
@@ -5,6 +5,6 @@ set -e
 
 mkdir -p output
 
-tar zcvf ./platforms/ios/build/emulator/volun-release-unsigned.app.tgz ./platforms/ios/build/emulator/Volun.app
+tar zcvf ./platforms/ios/build/emulator/volunteer-release-unsigned.app.tgz ./platforms/ios/build/emulator/Volun.app
 
-cp ./platforms/ios/build/emulator/volun-release-unsigned.app.tgz ./output/volun-release.app.tgz
+cp ./platforms/ios/build/emulator/volunteer-release-unsigned.app.tgz ./output/volunteer-release-unsigned.app.tgz
