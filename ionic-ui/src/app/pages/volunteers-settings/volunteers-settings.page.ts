1b8102fdcb687de33036f14af255ed35671061ca
brivil1 <brigitavilei@gmail.com>
2019-05-08 22:45:41 +0300

    *Linked volunteer settings page to back end* Added styling, changed structure, added entry form to html. Although still needs more styling

--- ionic-ui/src/app/pages/volunteers-settings/volunteers-settings.page.ts
+++ ionic-ui/src/app/pages/volunteers-settings/volunteers-settings.page.ts
@@ -6,6 +6,7 @@ import {User} from '../../models/User';
 import {UsersService} from '../../services/users.service';
 import {NavController} from '@ionic/angular';
 import {AlertController} from '@ionic/angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-volunteers-settings',
@@ -14,31 +15,30 @@ import {AlertController} from '@ionic/angular';
 })
 export class VolunteersSettingsPage implements OnInit {
 
-    user: User;
-    volunteer: Volunteer = {
-        id: 2,
-        firstName: 'Jason',
-        lastName: 'Statham',
-        phone: '1800-STATHAM',
-        email: 'baldman89@one.lt',
-        description: 'Anglų aktorius, geriausiai žinomas vaidmenimis Guy Ritchie kriminaliniuose filmuose „Lok, stok arba šauk“, „Revolveris“ ir „Vagišiai“',
-        pictureId: 21,
-        userId: 11,
-        projectsIds: [1],
-        imageUrl: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTUzMzQzOTkxMDAwMDgxNzA2/jason-statham-attends-the-press-conference-of-director-f-gary-grays-film-the-fate-of-the-furious-on-march-23-2017-in-beijing-china-photo-by-vcg_vcg-via-getty-images-square.jpg',
-        reviewsIds: [2],
-    };
+    user: number;
+    public onSaveForm: FormGroup;
+    volunteer: Volunteer = new Volunteer();
 
     constructor(
         private volunteersService: VolunteersService,
         private route: ActivatedRoute,
         private usersService: UsersService,
         private navCtrl: NavController,
+        private formBuilder: FormBuilder,
         public alertCtrl: AlertController) {
     }
 
     ngOnInit() {
-        this.user = this.usersService.getUser();
+        this.onSaveForm = this.formBuilder.group({
+            'imageUrl': [null, Validators.nullValidator],
+            'firstName':[null, Validators.nullValidator],
+            'lastName': [null, Validators.nullValidator],
+            'phone': [null, Validators.nullValidator],
+            'email': [null, Validators.nullValidator],
+            'description': [null, Validators.nullValidator],
+            'userId':this.usersService.getTokenId()
+          });
+        this.user = this.usersService.getTokenId();
         if (this.user === null) {
             this.navCtrl.navigateRoot('main').catch(e => console.log(e));
         }
@@ -47,7 +47,7 @@ export class VolunteersSettingsPage implements OnInit {
     }
 
     loadVolunteer() {
-        this.volunteersService.getByUsersId(this.user.id).subscribe(value => {
+        this.volunteersService.getByUsersId(this.user).subscribe(value => {
             this.volunteer = value;
         }, error1 => {
             console.log('Cannot get volunteer from database', error1);
@@ -55,7 +55,8 @@ export class VolunteersSettingsPage implements OnInit {
     }
 
     saveVolunteer() {
-        this.volunteersService.update(this.user.volunteerId, this.volunteer).subscribe(value => {
+        console.log(this.onSaveForm.value);
+        this.volunteersService.update(this.volunteer.id,this.onSaveForm.value).subscribe(value => {
             console.log('Volunteer was updated successfully');
             console.log(value);
 
