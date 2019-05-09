e65b68284792fea97891a35d4b1a8f1b4cee8f22
youstinus <youstinus@gmail.com>
2019-05-09 21:19:52 +0300

    #294 Created En and Lt language files to use strings from one place for whole project. Translated to english and Lithuanian language main page and side menu.

---
+++ ionic-ui/src/app/models/Lang.ts
@@ -0,0 +1,24 @@
+export class Lang {
+    // Side menu
+    public menuMain: string;
+    public menuOrganizations: string;
+    public menuProjects: string;
+    public menuSavedProjects: string;
+    public menuSelectedProjects: string;
+    public menuCreatedProjects: string;
+    public menuNewProject: string;
+    public menuCalendar: string;
+    public menuAboutUs: string;
+    public menuPrivacyPolicy: string;
+    public menuLogin: string;
+    public menuRegistration: string;
+    public menuSettings: string;
+    public menuMenu: string;
+    public menuLogout: string;
+    // Main page
+    public mainTitle: string;
+    public mainEvents: string;
+    public mainWantedOrganizations: string;
+
+    
+}
