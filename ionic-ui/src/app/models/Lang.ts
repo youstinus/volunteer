052730f595bf651b211ab7edf55636ddeef6abb0
Nihil <Nojus103@gmail.com>
2019-05-12 17:16:25 +0300

    implemented Lithuanian and english translation of copyright policy

--- ionic-ui/src/app/models/Lang.ts
+++ ionic-ui/src/app/models/Lang.ts
@@ -78,5 +78,56 @@ export class Lang {
     public registrationNotRegisteredHeader: string;
     public registrationNotRegisteredMessage: string;
     public registrationSuccess: string;
+    //Copyright page
+    public cr1: string;
+    public cr2: string;
+    public cr3: string;
+    public cr4: string;
+    public cr5: string;
+    public cr6: string;
+    public cr7: string;
+    public cr8: string;
+    public cr9: string;
+    public cr10: string;
+    public cr11: string;
+    public cr12: string;
+    public cr13: string;
+    public cr14: string;
+    public cr15: string;
+    public cr16: string;
+    public cr17: string;
+    public cr18: string;
+    public cr19: string;
+    public cr20: string;
+    public cr21: string;
+    public cr22: string;
+    public cr23: string;
+    public cr24: string;
+    public cr25: string;
+    public cr26: string;
+    public cr27: string;
+    public cr28: string;
+    public cr29: string;
+    public cr30: string;
+    public cr31: string;
+    public cr32: string;
+    public cr33: string;
+    public cr34: string;
+    public cr35: string;
+    public cr36: string;
+    public cr37: string;
+    public cr38: string;
+    public cr39: string;
+    public cr40: string;
+    public cr41: string;
+    public cr42: string;
+    public cr43: string;
+    public cr44: string;
+    public cr45: string;
+    public cr46: string;
+    public cr47: string;
+    public cr48: string;
+    public cr49: string;
+
 
 }
