59a9e72ccb243e204ec56d661160bed4c7d76917
Nihil <Nojus103@gmail.com>
2019-05-13 18:24:41 +0300

    about us page now passes unit tests

--- ionic-ui/src/app/pages/about/about.page.spec.ts
+++ ionic-ui/src/app/pages/about/about.page.spec.ts
@@ -1,8 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {Location, LocationStrategy, PathLocationStrategy, APP_BASE_HREF} from '@angular/common';
 import { AboutPage } from './about.page';
+import { HttpClient } from '@angular/common/http';
+import { HttpHandler } from '@angular/common/http';
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+import { Language } from 'src/app/utilities/Language';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { IonicModule } from '@ionic/angular';
 describe('AboutPage', () => {
   let component: AboutPage;
   let fixture: ComponentFixture<AboutPage>;
@@ -11,7 +18,12 @@ describe('AboutPage', () => {
     TestBed.configureTestingModule({
       declarations: [ AboutPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
+      imports: [ReactiveFormsModule, RouterTestingModule, IonicModule],
+      providers: [Location, LocationStrategy,HttpClient,HttpHandler,
+        StreamingMedia,Language, 
+      { provide: LocationStrategy, useClass: PathLocationStrategy },
+      //{ provide: APP_BASE_HREF, useValue: '/my/app'}
+          ]    })
     .compileComponents();
   }));
 
@@ -21,7 +33,10 @@ describe('AboutPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create component', () => {
+    expect(component instanceof AboutPage).toBe(true);
+  });
+  it('should have a class member', () => {
+    expect(component.menuAboutUs).toBeDefined();
   });
 });
