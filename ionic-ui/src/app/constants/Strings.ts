aa2c9679df0f9c9061ca754b4378617cdfab1c5a
Nihil <Nojus103@gmail.com>
2019-05-31 11:35:59 +0300

    changed video in main page and in about page due to my friend not wanting her voiced used outside of project

--- ionic-ui/src/app/constants/Strings.ts
+++ ionic-ui/src/app/constants/Strings.ts
@@ -5,7 +5,7 @@ export class Strings {
     public static Front_Page_Image = 'https://chicagocounseling.org/wp-content/uploads/2014/10/volunteering-is-fun-painted-hands.jpg';
     public static Color_Success = 'success';
     public static Color_Danger = 'danger';
-    public static Youtube_Video_Id = 'hJuojKuAtGA';
+    public static Youtube_Video_Id = '6w9TxnwVF5o';
     public static Send_Email_Address = `https://us-central1-volunteer-ui.cloudfunctions.net/sendMail`;
     public static Encryption_Key = 'J0bg8HQ8InMZl&yZWFq18nMl';
     public static Encryption_Iv = 'giK0vwUC';
@@ -13,6 +13,6 @@ export class Strings {
     public static About_Page_Source1 = 'https://www.gvi.co.uk/blog/17-excellent-reasons-to-volunteer/';
     public static About_Page_Source2 = 'https://buildabroad.org/2017/10/13/why-is-volunteering-important/';
     public static About_Page_Source3 = 'https://www.thebalancesmb.com/unexpected-benefits-of-volunteering-4132453';
-    public static Video_Url_Google_Drive = 'https://drive.google.com/uc?authuser=0&id=1oJOuOAgwUAi50_ZjShYiJR9Dh5ZPaTeK&export=download';
+    public static Video_Url_Google_Drive = 'https://drive.google.com/uc?authuser=0&id=1TuyGvPsiR63ybSS4Lxabt3pE9qiyQL64&export=download';
     public static About_Page_Image = 'https://broadviewthomsonpta.com/wp-content/uploads/2018/06/Volunteer.png';
 }
